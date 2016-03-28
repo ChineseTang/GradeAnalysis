88b82516729fb9bc85877bc243d2296daf12c66d
ChineseTang <tzjsmile@qq.com>
2016-03-28 14:36:45 +0800

    完成不同分辨率下的实现

--- src/com/ui/TitleLayout.java
+++ src/com/ui/TitleLayout.java
@@ -2,7 +2,6 @@ package com.ui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
